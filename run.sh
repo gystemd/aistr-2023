4281e08a2d3147d996fc1651753c3101bde45e2f
Cedric Willekens <cedric@willekens.dev>
2023-02-23 13:10:44 +0000

    Generate logs for the normal fuzzer

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 
 echo "Using file "$1"!"
 
+cp problems/$1.java $1.java
+
 java -cp target/aistr.jar nl.tudelft.instrumentation.Main --type=fuzzing --file=$1.java > instrumented/$1.java
 
 javac -cp target/aistr.jar:. Errors.java instrumented/$1.java
 
 java -cp target/aistr.jar:./instrumented:. $1
 
+rm $1.java
