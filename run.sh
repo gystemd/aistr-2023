13fd450b6fcf59c7f2331e4bede51fe9a0342b81
Cedric Willekens <cedric@willekens.dev>
2023-03-01 17:23:41 +0100

    Update run file

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 
-echo "Using file "$1"!"
+echo "Using file ""$1""!"
 
-cp problems/$1.java $1.java
+cp problems/"$1".java "$1".java
 
-java -cp target/aistr.jar nl.tudelft.instrumentation.Main --type=fuzzing --file=$1.java > instrumented/$1.java
+java -cp target/aistr.jar nl.tudelft.instrumentation.Main --type=symbolic --file="$1".java > instrumented/"$1".java
 
-javac -cp target/aistr.jar:. Errors.java instrumented/$1.java
+echo "Compiling"
 
-java -cp target/aistr.jar:./instrumented:. $1
+javac -cp target/aistr.jar:lib/com.microsoft.z3.jar:. Erros.java instrumented/"$1".java
+
+# javac -cp target/aistr.jar:. Errors.java instrumented/"$1".java
+
+echo "Running"
+
+java -cp target/aistr.jar:lib/com.microsoft.z3.jar:./instrumented "$1"
 
 rm $1.java
