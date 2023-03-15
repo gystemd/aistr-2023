7a5106454d0d3deb6245169c850e0255312ee490
Cedric Willekens <cedric@willekens.dev>
2023-03-15 15:07:53 +0100

    Calculate tarantual score and add buggy problems

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 
 echo "Using file ""$1""!"
 
-cp problems/"$1".java "$1".java
+cp buggy/"$1".java "$1".java
 
-java -cp target/aistr.jar nl.tudelft.instrumentation.Main --type=symbolic --file="$1".java > instrumented/"$1".java
+java -cp target/aistr.jar nl.tudelft.instrumentation.Main --type=patching --file="$1".java > instrumented/"$1".java
 
 echo "Compiling"
 
-javac -cp target/aistr.jar:lib/com.microsoft.z3.jar:. Errors.java instrumented/"$1".java
+#javac -cp target/aistr.jar:lib/com.microsoft.z3.jar:. Errors.java instrumented/"$1".java
 
-# javac -cp target/aistr.jar:. Errors.java instrumented/"$1".java
+javac -cp target/aistr.jar:. Errors.java instrumented/"$1".java
 
 echo "Running"
 
 java -cp target/aistr.jar:lib/com.microsoft.z3.jar:./instrumented:. "$1"
+
 
 rm "$1".java
--- src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java
+++ src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java
@@ -6,13 +6,18 @@ public class PatchingLab {
         static Random r = new Random();
         static boolean isFinished = false;
 
+        static Map<Integer, Set<Integer>> lineTouchedByTest = new HashMap<>();
+
+        static Map<Integer, Double> scoreForLine = new HashMap<>();
+
         static void initialize(){
                 // initialize the population based on OperatorTracker.operators
         }
 
         // encounteredOperator gets called for each operator encountered while running tests
         static boolean encounteredOperator(String operator, int left, int right, int operator_nr){
-                // Do something useful
+                updateCoverage(operator_nr);
+
 
                 String replacement = OperatorTracker.operators[operator_nr];
                 if(replacement.equals("!=")) return left != right;
@@ -26,11 +31,25 @@ public class PatchingLab {
 
         static boolean encounteredOperator(String operator, boolean left, boolean right, int operator_nr){
                 // Do something useful
+                updateCoverage(operator_nr);
+
 
                 String replacement = OperatorTracker.operators[operator_nr];
                 if(replacement.equals("!=")) return left != right;
                 if(replacement.equals("==")) return left == right;
                 return false;
+        }
+
+        static void updateCoverage(int operator_nr) {
+                // The tests which touched this operator (operator_nr)
+                Set<Integer> touchedByTest = new HashSet<>();
+                touchedByTest.add(OperatorTracker.current_test);
+                if (lineTouchedByTest.containsKey(operator_nr)) {
+                        touchedByTest.addAll(lineTouchedByTest.get(operator_nr));
+                        lineTouchedByTest.replace(operator_nr, touchedByTest);
+                } else {
+                        lineTouchedByTest.put(operator_nr, touchedByTest);
+                }
         }
 
         static void run() {
@@ -41,8 +60,27 @@ public class PatchingLab {
                 // Tests are loaded from resources/rers2020_test_cases. If you are you are using
                 // your own tests, make sure you put them in the same folder with the same
                 // naming convention.
-                OperatorTracker.runAllTests();
+                List<Boolean> testResults = OperatorTracker.runAllTests();
+                double numberOfTests = testResults.size();
+                System.out.println("Number of tests: " + numberOfTests);
+//                System.out.println(lineTouchedByTest);
                 System.out.println("Entered run");
+                lineTouchedByTest.forEach((line, tests) -> {
+                        int passed = 0;
+                        int failed = 0;
+                        for (Integer test : tests) {
+                                if (testResults.get(test)) {
+                                        passed++;
+                                } else {
+                                        failed++;
+                                }
+                        }
+                        double score =
+                                ((double) failed / numberOfTests) /
+                                        (((double) failed / numberOfTests) + ((double) passed / numberOfTests));
+                        scoreForLine.put(line, score);
+                });
+                System.out.println(scoreForLine);
 
                 // Loop here, running your genetic algorithm until you think it is done
                 while (!isFinished) {
